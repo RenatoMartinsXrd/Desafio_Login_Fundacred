4b7a11e370a4b593e21cabf7fd159afc42ac0bdb
Renato Martins de Almeida <renato.almeida@enjoei.com.br>
2022-08-03 00:49:44 -0300

    fix: prettier format

--- app/src/components/Form/Button.js
+++ app/src/components/Form/Button.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import styles from './Button.module.css'
-import classnames from 'classnames';
+import classnames from 'classnames'
 
 export const Button = ({ children, className }) => {
   const classes = classnames({
     [className]: true,
     [styles.btn]: true
-  });
+  })
   return <button className={classes}>{children}</button>
 }
